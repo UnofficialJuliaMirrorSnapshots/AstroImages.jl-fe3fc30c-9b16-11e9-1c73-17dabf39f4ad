86a95824723e27c76ce11cfea74ac735cc83d10c
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-08-28 09:11:56 +0000

    Snapshot of branch master taken on 2019-08-28T05:11:55.316-04:00 from "https://github.com/UnofficialJuliaMirror/AstroImages.jl-fe3fc30c-9b16-11e9-1c73-17dabf39f4ad"

--- src/AstroImages.jl
+++ src/AstroImages.jl
@@ -4,7 +4,7 @@ module AstroImages
 
 using FITSIO, FileIO, Images, Interact, Reproject, WCS, MappedArrays
 
-export load, AstroImage, ccd2rgb
+export load, AstroImage, ccd2rgb, set_brightness!, set_contrast!, add_label!, reset!
 
 _load(fits::FITS, ext::Int) = read(fits[ext])
 _load(fits::FITS, ext::NTuple{N, Int}) where {N} = ntuple(i-> read(fits[ext[i]]), N)
@@ -87,8 +87,14 @@ end
 
 mutable struct Properties{P <: Union{AbstractFloat, FixedPoint}}
     rgb_image::MappedArrays.MultiMappedArray{RGB{P},2,Tuple{Array{P,2},Array{P,2},Array{P,2}},Type{RGB{P}},typeof(ImageCore.extractchannels)}
+    contrast::Float64
+    brightness::Float64
+    label::Array{Tuple{Tuple{Float64,Float64},String},1}
     function Properties{P}(;kvs...) where P
         obj = new{P}()
+        obj.contrast = 1.0
+        obj.brightness = 0.0
+        obj.label = Array{Tuple{Tuple{Float64,Float64},String}}(undef,0)
         for (k,v) in kvs
             setproperty!(obj, k, v)
         end
@@ -98,6 +104,7 @@ end
 
 struct AstroImage{T<:Real,C<:Color, N, P}
     data::NTuple{N, Matrix{T}}
+    minmax::NTuple{N, Tuple{T,T}}
     wcs::NTuple{N, WCSTransform}
     property::Properties{P}
 end
@@ -109,26 +116,26 @@ end
 Construct an `AstroImage` object of `data`, using `color` as color map, `Gray` by default.
 """
 AstroImage(color::Type{<:Color}, data::Matrix{T}, wcs::WCSTransform) where {T<:Real} =
-    AstroImage{T,color, 1, Float64}((data,), (wcs,), Properties{Float64}())
+    AstroImage{T,color, 1, Float64}((data,), (extrema(data),), (wcs,), Properties{Float64}())
 function AstroImage(color::Type{<:AbstractRGB}, data::NTuple{N, Matrix{T}}, wcs::NTuple{N, WCSTransform}) where {T <: Union{AbstractFloat, FixedPoint}, N}
     if N == 3
         img = ccd2rgb((data[1], wcs[1]),(data[2], wcs[2]),(data[3], wcs[3]))
-        return AstroImage{T,color,N, widen(T)}(data, wcs, Properties{widen(T)}(rgb_image = img))
+        return AstroImage{T,color,N, widen(T)}(data, ntuple(i -> extrema(data[i]), N), wcs, Properties{widen(T)}(rgb_image = img))
     end
 end
 function AstroImage(color::Type{<:AbstractRGB}, data::NTuple{N, Matrix{T}}, wcs::NTuple{N, WCSTransform}) where {T<:Real, N}
     if N == 3
         img = ccd2rgb((data[1], wcs[1]),(data[2], wcs[2]),(data[3], wcs[3]))
-        return AstroImage{T,color,N, Float64}(data, wcs, Properties{Float64}(rgb_image = img))
+        return AstroImage{T,color,N, Float64}(data, ntuple(i -> extrema(data[i]), N), wcs, Properties{Float64}(rgb_image = img))
     end
 end
 function AstroImage(color::Type{<:Color}, data::NTuple{N, Matrix{T}}, wcs::NTuple{N, WCSTransform}) where {T<:Real, N}
-    return AstroImage{T,color, N, Float64}(data, wcs, Properties{Float64}())
+    return AstroImage{T,color, N, Float64}(data, ntuple(i -> extrema(data[i]), N), wcs, Properties{Float64}())
 end
-AstroImage(data::Matrix{T}) where {T<:Real} = AstroImage{T,Gray,1, Float64}((data,), (WCSTransform(2),), Properties{Float64}())
-AstroImage(data::NTuple{N, Matrix{T}}) where {T<:Real, N} = AstroImage{T,Gray,N, Float64}(data, ntuple(i-> WCSTransform(2), N), Properties{Float64}())
-AstroImage(data::Matrix{T}, wcs::WCSTransform) where {T<:Real} = AstroImage{T,Gray,1, Float64}((data,), (wcs,), Properties{Float64}())
-AstroImage(data::NTuple{N, Matrix{T}}, wcs::NTuple{N, WCSTransform}) where {T<:Real, N} = AstroImage{T,Gray,N, Float64}(data, wcs, Properties{Float64}())
+AstroImage(data::Matrix{T}) where {T<:Real} = AstroImage{T,Gray,1, Float64}((data,), (extrema(data),), (WCSTransform(2),), Properties{Float64}())
+AstroImage(data::NTuple{N, Matrix{T}}) where {T<:Real, N} = AstroImage{T,Gray,N, Float64}(data, ntuple(i -> extrema(data[i]), N), ntuple(i-> WCSTransform(2), N), Properties{Float64}())
+AstroImage(data::Matrix{T}, wcs::WCSTransform) where {T<:Real} = AstroImage{T,Gray,1, Float64}((data,), (extrema(data),), (wcs,), Properties{Float64}())
+AstroImage(data::NTuple{N, Matrix{T}}, wcs::NTuple{N, WCSTransform}) where {T<:Real, N} = AstroImage{T,Gray,N, Float64}(data, ntuple(i -> extrema(data[i]), N), wcs, Properties{Float64}())
 
 """
     AstroImage([color=Gray,] filename::String, n::Int=1)
@@ -159,13 +166,72 @@ AstroImage(color::Type{<:Color}, files::NTuple{N,String}) where {N} =
     AstroImage(color, load(files)...)
 AstroImage(file::String) = AstroImage((file,))
 
-# Lazily render the image as a Matrix{Color}, upon request.
+# Lazily reinterpret the image as a Matrix{Color}, upon request.
 function render(img::AstroImage{T,C,N}, header_number = 1) where {T,C,N}
-    imgmin, imgmax = extrema(img.data[header_number])
+    imgmin, imgmax = extrema(img.minmax[header_number])
     # Add one to maximum to work around this issue:
     # https://github.com/JuliaMath/FixedPointNumbers.jl/issues/102
     f = scaleminmax(_float(imgmin), _float(max(imgmax, imgmax + one(T))))
     return colorview(C, f.(_float.(img.data[header_number])))
+end
+
+"""
+    set_brightness!(img::AstroImage, value::AbstractFloat)
+
+Sets brightness of `rgb_image` to value.
+"""
+function set_brightness!(img::AstroImage, value::AbstractFloat)
+    if isdefined(img.property, :rgb_image)
+        diff = value - img.property.brightness
+        img.property.brightness = value
+        img.property.rgb_image .+= RGB{typeof(value)}(diff, diff, diff)
+    else
+        throw(DomainError(value, "Can't apply operation. AstroImage dosen't contain :rgb_image"))
+    end
+end
+
+"""
+    set_contrast!(img::AstroImage, value::AbstractFloat)
+
+Sets contrast of rgb_image to value.
+"""
+function set_contrast!(img::AstroImage, value::AbstractFloat)
+    if isdefined(img.property, :rgb_image)
+        diff = (value / img.property.contrast)
+        img.property.contrast = value
+        img.property.rgb_image = colorview(RGB, red.(img.property.rgb_image) .* diff, green.(img.property.rgb_image) .* diff,
+                                                blue.(img.property.rgb_image) .* diff)
+    else
+        throw(DomainError(value, "Can't apply operation. AstroImage dosen't contain :rgb_image"))
+    end
+end
+
+"""
+    add_label!(img::AstroImage, x::Real, y::Real, label::String)
+
+Stores label to coordinates (x,y) in AstroImage's property label.
+"""
+function add_label!(img::AstroImage, x::Real, y::Real, label::String)
+    push!(img.property.label, ((x,y), label))
+end
+
+"""
+    reset!(img::AstroImage)
+
+Resets AstroImage property fields.
+
+Sets brightness to 0.0, contrast to 1.0, empties label
+and form a fresh rgb_image without any brightness, contrast operations on it.
+"""
+function reset!(img::AstroImage{T,C,N}) where {T,C,N}
+    img.property.contrast = 1.0
+    img.property.brightness = 0.0
+    img.property.label = []
+    if N == 3 && C == RGB
+        shape_out = size(img.property.rgb_image)
+        img.property.rgb_image = ccd2rgb((img.data[1], img.wcs[1]),(img.data[2], img.wcs[2]),(img.data[3], img.wcs[3]),
+                                            shape_out = shape_out)
+    end
 end
 
 Images.colorview(img::AstroImage) = render(img)
